4df51c2e337a339c216c80cc24cfcd0669519079
aryan singh <singharyan5656@gmail.com>
2024-02-27 21:19:45 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+ // alert(3+3) we are using nodejs not browser
+  
+ let name="aryan"
+ let age=10
+ let logedIn=false
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+  // boolean=> true/false
+  // null=> standalone value
+  // undefined => value is not assigned
+  // symbol=>for the uniqueness
+
+
+
+  // object
+
+
+  console.log( typeof"aryan")
---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId=14126
+let accountEmail="aryan@gmail.com"
+var accountPassword="12345"
+accountcity="bengaluru"
+accountEmail="aryan.com"
+accountPassword="221020"
+ console.log(accountId);
+ console.table([accountId, accountEmail, accountPassword])
+
+
+ /*
+
+ prefer not to use var 
+ beacuse of issue in block scope  and functional scope
+ */
