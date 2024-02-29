7c565f508f97f6334030a5cd44a60ef4e8ea4a57
aryan singh <singharyan5656@gmail.com>
2024-02-29 23:12:23 +0530

    math in javascript

---
+++ 01-basics/nums_and_maths.js
@@ -0,0 +1,18 @@
+const score= 400
+const balance= new number(100)
+console.log(balance)
+
+console.log(balance.toString(). length);
+console.log( balance.toFixed(1));
+const otherNumber= 23.43455
+console.log(otherNumber.toPrecision(3))
+//********************************************** */
+// maths
+
+console.log(Maths.abs(-4)) // simply make the number positive
+console.log(Maths.round(4.4))
+ console.log(Maths.ceil(4.6))
+
+
+
+  console.log(Maths.floor(Maths.random()*10)+1)
