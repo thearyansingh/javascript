aefdcfe7b191c2c807edf19318f26cdfc405c16f
aryan singh <singharyan5656@gmail.com>
2024-03-02 23:52:35 +0530

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,34 @@
+// function addNumber(num1,num2){
+//  return num1 +num2
+// }
+// const result=addNumber(3,5)
+// console.log(result)
+
+// function loginUser(userName){
+//     if(userName===undefined){
+//         console.log("please enetr userName")
+//         return  
+//      }
+//      return`${userName} just logged in`
+   
+
+// }
+const user={
+    username:"aryan",
+    price:"199"
+}
+function handle(user){
+console.log(`username is${user.username} and price is ${user.price}`)
+
+}
+handle(user)
+
+handle({
+    username:"sam",
+    price:399
+})
+const myNewArray=[200,400,100,600]
+function returnSecond(getArray){
+return getArray[1]
+}
+console.log(returnSecond(myNewArray))
