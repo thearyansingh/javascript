53a97096630a8d17324fc35973697d0c003c394a
thearyansingh <singharyan6999@gmail.com>
2024-04-05 21:40:29 +0530

    asyn & await

---
+++ 07_events/four.js
@@ -0,0 +1,11 @@
+const randomColor=()=>{
+    const hex="0123456789ABCDEF"
+    let color="#"
+    for(let i=0;i<6;i++){
+      color+=hex[Math.floor(Math.random() * 16)]
+     
+    }
+    return color;
+ 
+  };
+  console.log(randomColor());
